15b86032deb416c16a2138274c479acaa97033ea
coderkhushal <137866759+coderkhushal@users.noreply.github.com>
2024-03-29 16:44:16 +0530

    dashboard changes

--- app/(dashboard)/components/Detailscard.tsx
+++ app/(dashboard)/components/Detailscard.tsx
@@ -1,8 +1,25 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { ScrollArea } from "@/components/ui/scroll-area"
 import React from 'react'
-
-const Detailscard = () => {
+interface DetailsCardProps{
+    heading : string
+}
+const Detailscard = ({heading}: DetailsCardProps) => {
   return (
-    <div>Detailscard</div>
+    <Card className='shadow-lg bg-gray-50'>
+        <CardHeader>
+            <CardTitle className='w-full text-center'>
+                Bank/Card Details
+            </CardTitle>
+        </CardHeader>
+        <CardContent>
+            <ScrollArea className='h-80'>
+              
+            </ScrollArea>
+        
+        </CardContent>
+    </Card>
+
   )
 }
 
--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -4,16 +4,15 @@ import CreationCard from '../components/creationcard'
 import Detailscard from '../components/Detailscard'
 const DashBoardPage = () => {
     return (
-        <div className='w-full gap-10 flex flex-col'>
-            <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
+        <div className='w-full gap-10 h-full flex flex-col'>
+            <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3 w-full'>
                 <CreationCard heading='Add Bank/Card' details='you have currently 3 bank accounts' />
                 <CreationCard heading='Add Investment/Purchases' details='you have currently 3 bank accounts' />
                 <CreationCard heading='Add Loan/Emi' details='you have currently 3 bank accounts' />
-                <CreationCard heading='Add Income' details='you have currently 3 bank accounts' />
             </div>
 
-            <div >
-                <Detailscard/>
+            <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4 w-full'>
+                <Detailscard heading="Bank/Card Details"/>
             </div>
 
 
