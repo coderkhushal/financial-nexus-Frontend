8ca04a67a5198209a29c79125069bc2c8f873853
coderkhushal <137866759+coderkhushal@users.noreply.github.com>
2024-04-01 15:24:00 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-        async rewrites() {
-          return [
-            {
-              source: '/:path*',
-              destination: 'http://localhost:4000/:path*'
-            }
-          ]
-        }
       
 };
 
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,3 @@
 module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/:path*',
-          destination: 'https://financial-nexus-backend.yellowbush-cadc3844.centralindia.azurecontainerapps.io/:path*'
-        }
-      ]
-    }
+
   }
