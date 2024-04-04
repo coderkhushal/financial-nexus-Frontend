9d77f33912a5cd06b142d50535e08ef4420da276
coderkhushal <137866759+coderkhushal@users.noreply.github.com>
2024-04-05 01:36:42 +0530

    changes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -16,31 +16,27 @@ const Sidebar = () => {
             className="sidebar h-full p-2 w-full relative  text-center bg-gray-900"
         >
             <div className="text-gray-100 text-xl ">
-                <Link href="/">
-                    <div className=" cursor-pointer p-2.5 mt-1 flex items-center h-full">
-                        <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
-                        <h1 className="font-bold text-gray-200 text-[15px] ml-3">Financial Nexus</h1>
-                        <i
-                            className="bi bi-x cursor-pointer ml-28 lg:hidden"
+                <div className=" cursor-pointer p-2.5 mt-1 flex items-center h-full">
+                    <Link href="/">
+                        <h1 className="font-bold text-gray-200 text-[17px] text-start ">Financial Nexus</h1>
+                    </Link>
 
-                        ></i>
-                    </div>
-                </Link>
+                </div>
                 <div className="my-2 bg-gray-600 h-[1px]"></div>
             </div>
-            { routes.map((route, index) => (
+            {routes.map((route, index) => (
                 <Link href={route.route} key={index} className='w-full' >
                     <Button className='w-full space-x-0 p-0 my-2' >
 
 
                         <div
                             className={`p-2.5 mt-3 flex w-full  rounded-md  duration-300 cursor-pointer ${pathname == route.route && "bg-gray-600"} justify-start hover:bg-blue-600 text-white`}
-                            >
-                            <route.icon/>
+                        >
+                            <route.icon />
                             <span className="text-[15px]  w-full text-gray-200 font-bold">{route.name}</span>
                         </div>
-                    
-                            </Button>
+
+                    </Button>
                 </Link>
             ))}
 
